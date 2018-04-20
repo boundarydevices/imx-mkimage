e7e63764fa97ae94b0564f5e8f11a0d1556033e0
Gary Bisson <gary.bisson@boundarydevices.com>
2018-04-20 12:56:53 +0200

    make_boundary.sh: update to match new soc.mak
    Also copy the U-Boot mkimage tool.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- make_boundary.sh
+++ make_boundary.sh
@@ -3,8 +3,9 @@ export CROSS_COMPILE=aarch64-linux-gnu-
 cp -v ../u-boot-imx6/spl/u-boot-spl.bin iMX8M/
 cp -v ../u-boot-imx6/u-boot-nodtb.bin iMX8M/
 cp -v ../u-boot-imx6/arch/arm/dts/imx8mq-nitrogen8m.dtb iMX8M/
+cp -v ../u-boot-imx6/tools/mkimage iMX8M/mkimage_uboot
 make clean
-make SOC=iMX8M flash_hdmi_spl_uboot_nitrogen8m
+make SOC=iMX8M DTBS=imx8mq-nitrogen8m.dtb flash_hdmi_spl_uboot
 
 echo "Next: dd if=iMX8M/flash.bin of=/dev/sd[x] bs=1K seek=33 skip=0"
 
