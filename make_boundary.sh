597e03f0787d7ca46ed69c2fc3fb19fff97c5f1d
Gary Bisson <gary.bisson@boundarydevices.com>
2018-07-03 16:31:37 +0200

    make_boundary.sh: allow overriding hardcoded path and dtb name
    By setting UBOOT_PATH and/or UBOOT_DTB like:
    UBOOT_PATH=/mysecretpath UBOOT_DTB=imx8mq-secret.dtb ./make_boundary.sh
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- make_boundary.sh
+++ make_boundary.sh
@@ -1,13 +1,28 @@
+#!/bin/sh
+
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
-cp -vt iMX8M/ ../u-boot-imx6/spl/u-boot-spl.bin \
-	../u-boot-imx6/u-boot-nodtb.bin \
-	../u-boot-imx6/u-boot.bin \
-	../u-boot-imx6/arch/arm/dts/imx8mq-nitrogen8m.dtb
-cp -v ../u-boot-imx6/tools/mkimage iMX8M/mkimage_uboot
+
+if [ -z ${UBOOT_PATH} ]; then
+	UBOOT_PATH=../u-boot-imx6
+fi
+
+if [ -z ${UBOOT_DTB} ]; then
+	UBOOT_DTB=imx8mq-nitrogen8m.dtb
+fi
+
+cp -vt iMX8M/ ${UBOOT_PATH}/spl/u-boot-spl.bin \
+	${UBOOT_PATH}/u-boot-nodtb.bin \
+	${UBOOT_PATH}/u-boot.bin \
+	${UBOOT_PATH}/arch/arm/dts/${UBOOT_DTB}
+cp -v ${UBOOT_PATH}/tools/mkimage iMX8M/mkimage_uboot
+if ! [ $? -eq 0 ] ; then
+	echo "Failed to copy files from ${UBOOT_PATH}";
+	exit 1;
+fi
+
 make clean
-make SOC=iMX8M DTBS=imx8mq-nitrogen8m.dtb flash_hdmi_spl_uboot
+make SOC=iMX8M DTBS=${UBOOT_DTB} flash_hdmi_spl_uboot
 
-cd iMX8M && ./print_fit_hab.sh 0x60000 imx8mq-nitrogen8m.dtb ; cd ..
+cd iMX8M && ./print_fit_hab.sh 0x60000 ${UBOOT_DTB} ; cd ..
 echo "Next: dd if=iMX8M/flash.bin of=/dev/sd[x] bs=1K seek=33 skip=0"
-
